48d2bf114cc8b0be4ebe82a42a0356aba6982648
Karthika Rajagopal <78952344+Karthika-Rajagopal@users.noreply.github.com>
2023-05-06 12:36:00 +0530

    Update load_env.go

--- config/load_env.go
+++ config/load_env.go
@@ -1,11 +1,11 @@
 package config
 
 import(
-	"github.com/spf13/viper"
+	"github.com/spf13/viper"   //this package is to load the configuration from an environment variable file
 )
 
 type Config struct{
-	//MySQL Setup
+	//MySQL Setup, defines the configuration options for the application, which include MySQL and Redis setup details
 	DBHost string `mapstructure:"SQL_HOST"`
 	DBUsername string `mapstructure:"SQL_USER"`
 	DBPassword string `mapstructure:"SQL_PASSWORD"`
@@ -17,7 +17,7 @@ type Config struct{
 
 }
 
-func LoadConfig(path string)(config Config, err error){
+func LoadConfig(path string)(config Config, err error){  //path to the environment variable file as a parameter
 	viper.AddConfigPath(path)
 	viper.SetConfigType("env")
 	viper.SetConfigName("app")
@@ -25,7 +25,7 @@ func LoadConfig(path string)(config Config, err error){
 	viper.AutomaticEnv()
 
 	//handle null
-	err = viper.ReadInConfig()
+	err = viper.ReadInConfig()  /// package to read the configuration from the environment variable file, unmarshal it into the Config struct, and return it along with any errors
 	if err!=nil{
 		return
 	}
